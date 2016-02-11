db8eb9f0699ff041772d8fef25e084b7037d2535
Johannes Rudolph <johannes.rudolph@gmail.com>
2016-02-11 12:28:19 +0100

    add cross-building to 2.12.0-M3

--- project/ReifyBuild.scala
+++ project/ReifyBuild.scala
@@ -20,13 +20,18 @@ object ReifyBuild extends Build {
         publishArtifact := false,
         libraryDependencies <+= scalaVersion("org.scala-lang" % "scala-reflect" % _ % "provided"),
         libraryDependencies ++= Seq(
-          "org.specs2" %%  "specs2-core" % "3.7" % "test"
+          ("org.specs2" %%  "specs2-core" % "3.7" % "test")
+            .exclude( "org.scalaz", "scalaz-concurrent_2.12.0-M2" )
+            .exclude( "org.scalaz", "scalaz-effect_2.12.0-M2" )
+            .exclude( "org.scalaz", "scalaz-core_2.12.0-M2" )
+            .exclude( "org.scala-lang.modules", "scala-xml_2.12.0-M2" )
+            .exclude( "org.scala-lang.modules", "scala-parser-combinators_2.12.0-M2" )
         )
       )
       .dependsOn(reify)
 
   def defaultSettings = Seq(
-    scalaVersion := "2.11.7",
+    crossScalaVersions := Seq("2.11.7", "2.12.0-M3"),
     scalacOptions ++= Seq("-language:_", "-deprecation", "-unchecked"),
 
     organization := "net.virtual-void",
