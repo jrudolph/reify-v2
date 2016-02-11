6822abec7dbdc4aad3a3d445f3ff2bb32a2dbf8f
Johannes Rudolph <johannes.rudolph@gmail.com>
2016-02-11 12:28:38 +0100

    add a fast list constructor

--- reify-test/src/main/scala/net/virtualvoid/reify2/FastArray.scala
+++ reify-test/src/main/scala/net/virtualvoid/reify2/FastArray.scala
@@ -1,11 +1,14 @@
 package net.virtualvoid.reify2
 
+import scala.annotation.tailrec
 import scala.reflect.ClassTag
 import scala.reflect.macros.blackbox
 
 object FastArray {
   def apply[T: ClassTag](els: T*): Array[T] = macro impl[T]
   def naive[T: ClassTag](els: T*): Array[T] = macro naiveImpl[T]
+
+  def list[T](els: T*): List[T] = macro listImpl[T]
 
   def naiveImpl[T: ctx.WeakTypeTag](ctx: blackbox.Context)(els: ctx.Expr[T]*)(tTag: ctx.Expr[ClassTag[T]]): ctx.Expr[Array[T]] =
     new Reifier {
@@ -37,5 +40,19 @@ object FastArray {
 
           res
         }
+    }.run
+
+  def listImpl[T: ctx.WeakTypeTag](ctx: blackbox.Context)(els: ctx.Expr[T]*): ctx.Expr[List[T]] =
+    new Reifier {
+      val c: ctx.type = ctx
+      def run: c.Expr[List[T]] = {
+        @tailrec def consAll(els: Seq[ctx.Expr[T]], result: Expr[List[T]]): Expr[List[T]] =
+          els match {
+            case head +: tail ⇒ consAll(tail, reify(head.splice :: result.splice))
+            case Nil          ⇒ result
+          }
+
+        consAll(els.reverse, reify(Nil))
+      }
     }.run
 }
